57b6786beef2f88882a68346fd3407a95f4c125b
nikhil <nikhilrajpurohitnkp@gmail.com>
2024-11-25 02:44:22 +0530

    middleware setup for errorHandleing

---
+++ constants.js
@@ -0,0 +1,7 @@
+exports.constants = {//all the server side error and theier easy to place in name : this was extra but design choice to make it more readable
+    VALIDATION_ERROR: 400,
+    UNAUTHORIZED: 401,
+    FORBIDDEN:402,
+    NOT_FOUND:404,
+    SERVER_ERROR:500
+}
