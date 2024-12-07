1cc911ec9dc68955ac39f4dd89d003b765ae02b3
nikhil <nikhilrajpurohitnkp@gmail.com>
2024-12-08 02:42:52 +0530

    added all the essentail CRU routes on student/faculty

--- routes/complaintRoutes.js
+++ routes/complaintRoutes.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../middleware/authMiddleware');
-const { createComplaint, getAssignedComplaints,readComplaints } = require('../controller/complaintController');
+const {  
+        createComplaint,
+        getAssignedComplaints,
+        readComplaints ,
+        getPendingComplaints,
+        addressPendingComplaints,
+        getResolvedComplaints,
+        getRejectedComplaints} = require('../controller/complaintController');
 
 // Routes
-router.post('/', protect, createComplaint); // Student creates a complaint
-router.get('/pending', protect, getAssignedComplaints); // Faculty views assigned complaints
+
 router.get('/history',protect,readComplaints) //api/complaints/history to see all the complaints by/to the student/faculty
+
+    //student
+    router.post('/', protect, createComplaint); // Student creates a complaint
+    router.get('/unsolved', protect, getPendingComplaints); // student views  complaints that are unsolved yet
+    router.get('/resolved', protect, getResolvedComplaints); // student views  complaints that are solved and accepted
+    router.get('/rejected', protect, getRejectedComplaints); // student views  complaints that are solved and rejected
+
+
+
+    //faculty
+    router.get('/pending', protect, getAssignedComplaints); // Faculty views assigned complaints
+    router.put('/pending/:id',protect,addressPendingComplaints); //review the complaint and address it 
+
+
 
 
 module.exports = router;
