38dfd9d7f5c8d7bd3267be9a56bf0f40742b51c0
nikhil <nikhilrajpurohitnkp@gmail.com>
2024-12-08 01:11:43 +0530

    added some more dependencies

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -5,6 +5,7 @@ const connectDb = async () =>{
     try {
         const connect = await mongoose.connect(process.env.CONNECTION_STRING);
         console.log("db connected",connect.connection.host,connect.connection.name)
+        console.log("geminiapi",process.env.GEMINI)
     } catch (err) {
         console.log(err);
         process.exit(1);
